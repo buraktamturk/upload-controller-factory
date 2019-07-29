b2e4818362bc8fa06e69b0a9370e4656f29dc626
Burak Tamturk <buraktamturk@gmail.com>
2019-07-29 15:15:40 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# UploadController
+
+JavaScript file uploading mechanism with default (optional) XHR backend. To be consumed by file picker and image picker components.
+
+## Usage
+
+```javascript
+
+  var UploadControllerFactory = require('uploadcontroller');
+
+  var UploadController = UploadControllerFactory({
+    xhrUrl: 'http://localhost/upload.php?filename=$file', // you can use $file to put file name to the URL
+    xhrAsFormData: true, // if true, it sends the file as multipart data. Otherwise the whole file is sent as a body with correct mime type
+    xhrFormDataFileKey: 'file', // if xhrAsFormData true, 
+
+    xhrMethod: 'POST', // default
+    xhrHeaders: {}, // extra headers to be sent
+    xhrCallback: function(xhr) { }, // XHR hook before sending data
+    upload: function(name, blob, contentType, abort, progress) {
+      // Disables XHR and enables custom uploading backend.
+      // Function must return a promise that will resolve with a javascript object.
+      // when the user aborts the uploading, the AbortSignal is fired.
+      // upload progress can be reported back to library by calling progress function which accepts numbers from 0 to 1 (i.e. progress(50 / 100) means half of the upload is completed).
+    },
+
+    valueKey: 'id', 
+    srcKey: 'access_url', 
+
+    fields: ['id', 'access_url', 'name', 'size'] // save additional data from response, so they can be accessed from JavaScript with UploadController inside
+  });
+
+  var file1 = UploadController();
+
+  // attach file1 to file picker component or image component
+  // or
+
+  file1.upload(name, contentType, blob); // start upload selected file
+  // returns promise on finish
+
+  // file1.$state = 1 is empty, 2 is uploading, 3 is finished
+  // file1.$pr = Upload progress report: 0 is 0% percent, 1 is 100% percent
+  // file1.$blob_url = Locally cached Blob resource, can be used in <img> tags or to display a PDF in a new window.
+
+  file1.addListener(function() {
+    // track the controller
+  });
+
+  // defines already uploaded file
+  var file2 = UploadController({ id: 1, access_url: 'https://www.gstatic.com/webp/gallery/2.jpg' });
+  // can be attached to file picker component or image component, and the component will show the previously selected file.
+```
+
+## License
+
+© 2019 [Burak Tamturk](https://buraktamturk.org/)
+
+Released under the [MIT LICENSE](http://opensource.org/licenses/MIT)
---
+++ index.js
@@ -0,0 +1,210 @@
+(function(root, factory) {
+  if(typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if(typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    root.UploadControllerFactory = factory();
+  }
+})(this, function() {
+  return function(config) {
+    var valueKey = (config && config.valueKey) || 'id';
+    var srcKey = (config && config.srcKey) || 'access_url';
+
+    var fields = config.fields || ['id', 'access_url', 'name', 'size'];
+
+    var uploadFnc = config.uploader;
+    if(!uploadFnc) {
+      var xhrMethod = config.xhrMethod || 'POST';
+
+      if(!config.xhrUrl) {
+        throw new Error('Please provide URL.')
+      }
+
+      var xhrUrl = config.xhrUrl;
+      var xhrAsFormData = config.xhrAsFormData || false;
+      var xhrFormDataNameKey = config.xhrFormDataNameKey;
+      var xhrFormDataFileKey = config.xhrFormDataFileKey || 'file';
+      var xhrCallback = config.xhrCallback;
+    
+      uploadFnc = async function(name, blob, contentType, abort, progress) {
+        return await new Promise(function(resolve, reject) {
+          var xhr = new XMLHttpRequest();
+
+          abort.onabort = function() {
+            xhr.onreadystatechange = null;
+            xhr.abort();
+            reject(new Error('Aborted!'));
+          };
+    
+          xhr.upload.onprogress = xhr.upload.onloadstart = function(e) {
+            progress(e.loaded / e.total);
+          };
+    
+          xhr.onreadystatechange = function(e) {
+            if (this.readyState === XMLHttpRequest.DONE) {
+              if(this.status === 200) {
+                try {
+                  var data = JSON.parse(this.responseText);
+                  if(data == null)
+                    reject();
+                  else
+                    resolve(data);
+                } catch(e) {
+                  reject(e);
+                }
+              } else {
+                reject(this);
+              }
+            }
+          };
+
+          xhr.open(xhrMethod, xhrUrl.replace('$file', encodeURIComponent(name)), true);
+    
+          if(xhrCallback) {
+            xhrCallback(xhr);
+          }
+    
+          if(xhrAsFormData) {
+            var formData = new FormData();
+            xhrFormDataNameKey && formData.append(xhrFormDataNameKey, name);
+            formData.append(xhrFormDataFileKey, blob, name);
+            xhr.send(formData);
+          } else {
+            xhr.setRequestHeader("Content-Type", contentType);
+            xhr.send(blob);
+          }
+        });
+      };
+    }
+
+    class UploadController {
+      constructor(data) {
+        this.listeners = [];
+
+        if(data) {
+          for(var key of fields) {
+            this[key] = data[key];
+          }
+        }
+
+        this.$state = this[valueKey] ? 3 : 1;
+      }
+
+      async upload(name, contentType, blob) {
+        await this.cancel();
+
+        this.name = name;
+
+        var previous = {};
+
+        for(var key of fields) {
+          previous[key] = this[key];
+        }
+
+        previous.$state = this.$state;
+        previous.$blob = this.$blob;
+        previous.$blob_url = this.$blob_url;
+
+        this.$abort = new AbortController();
+
+        try {
+            this.$blob = blob;
+            this.$blob_url = URL.createObjectURL(blob);
+            this.$state = 2;
+
+            this.$pr = 0;
+
+            this.notify();
+
+            var data = await uploadFnc(name, blob, contentType, this.$abort.signal, (a) => {
+              this.$pr = a;
+              this.notify();
+            });
+
+            this.$pr = 1;
+
+            this.$state = 3;
+
+            this.notify();
+            
+            for(var key of fields) {
+              this[key] = data[key];
+            }
+        } catch (e) {
+          for(var key of fields) {
+            this[key] = previous[key];
+          }
+
+          this.$state = previous.$state;
+          this.$blob = previous.$blob;
+          this.$blob_url = previous.$blob_url;
+
+          this.notify();
+        } finally { 
+          this.$abort = null;
+        }
+      }
+
+      notify() {
+        for(var i = 0; i < this.listeners.length; ++i) {
+          (this.listeners[i])(this);
+        }
+      }
+
+      async remove() {
+        await this.cancel();
+
+        this.$blob = null;
+        this.$blob_url = null;
+        this.$state = 1;
+        this.notify();
+      }
+
+      async cancel() {
+        if (this.$abort) {
+          var task = this.$lastTask;
+          this.$abort.abort();
+          await task;
+        }
+      }
+
+      addListener(listener) {
+        if(this.listeners.indexOf(listener) == -1) {
+          this.listeners.push(listener);
+          listener(this);
+        } else {
+          throw new Error('Same listener is connected.');
+        }
+      }
+
+      removeListener(listener) {
+        var index = this.listeners.indexOf(listener);
+        if(index == -1) {
+          this.listeners.splice(index, 1);
+        } else {
+          throw new Error('Listener is not connected');
+        }
+      }
+
+      toJSON() {
+        var data = {};
+
+        for(var key in fields) {
+          data[key] = this[key];
+        }
+
+        return data;
+      }
+    };
+
+    var fnc = function(data) {
+      return data && (data instanceof UploadController) ? data : new UploadController(data);
+    };
+
+    fnc.valueKey = valueKey;
+    fnc.srcKey = srcKey;
+
+    return fnc;
+  };
+});
